ba748d0911cf5831b39682855d318d7e81dfd2a3
rick-03 <satwick.edu@gmail.com>
2024-03-01 00:38:42 +0530

    travel webpage done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Destinations from "./components/Destinations";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import Offer from "./components/Offer";
+import Services from "./components/Services";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <Home />
+      <Services />
+      <Destinations />
+      <Offer />
     </div>
   );
 }
-
-export default App;
