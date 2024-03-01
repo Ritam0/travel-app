14c362f1c8f1c90a48655afa16d461d503aeab11
Ritam Majumder <ritmmajumder0@gmail.com>
2024-03-02 01:37:50 +0530

    ok

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import React from "react";
-import Destinations from "./components/Destinations";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
-import Offer from "./components/Offer";
-import Services from "./components/Services";
 
 export default function App() {
   return (
     <div>
       <Navbar />
       <Home />
-      <Services />
-      <Destinations />
-      <Offer />
+      
     </div>
   );
 }
